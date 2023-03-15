496cef3df7344b9d00980b89a82fbe9e8edc433e
siya dessai <21833.siya.sdcce@gmail.com>
2023-03-15 15:22:57 +0530

    modified salarytest

--- SalaryTest.java
+++ SalaryTest.java
@@ -7,38 +7,38 @@ import org.junit.jupiter.api.Test;
 class SalaryTest {
 	 @Test
 	    void testDA() {
-	        Salary salary = new Salary(100000);
-	        assertEquals(73000.0, salary.getDA());
+	        Salary s = new Salary(100000);
+	        assertEquals(73000.0, s.getDA());
 	       
 
 	      
 	    }
 	    @Test
 	    void testHRA() {
-	        Salary salary = new Salary(100000);
-	        assertEquals(10000.0, salary.getHRA());
+	        Salary s = new Salary(100000);
+	        assertEquals(10000.0, s.getHRA());
 	       
 	      
 	    }
 	    @Test
 	    void testGrossSalary() {
-	        Salary salary = new Salary(100000);  
-	        assertEquals(183000.0, salary.getGrossSalary());
+	        Salary s = new Salary(100000);  
+	        assertEquals(183000.0, s.getGrossSalary());
 	        
 	      
 	    }
 	    @Test
 	    void testIncomeTex() {
-	        Salary salary = new Salary(100000);
-	        assertEquals(54900.0, salary.getIncomeTax());
+	        Salary s = new Salary(100000);
+	        assertEquals(54900.0, s.getIncomeTax());
 	       
 
 	      
 	    }
 	    @Test
 	    void testNetSalary() {
-	        Salary salary = new Salary(100000);
-	        assertEquals(128100.0, salary.getNetSalary());
+	        Salary s = new Salary(100000);
+	        assertEquals(128100.0, s.getNetSalary());
 
 	}
 	}
